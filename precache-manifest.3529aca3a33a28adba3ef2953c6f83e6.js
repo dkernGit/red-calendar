26ee131943a0e51f2eec92c278ecae4e4069c8aa
dkernGit <snowpunk@gmail.com>
2020-02-02 13:08:23 -0800

    Updates

---
+++ precache-manifest.3529aca3a33a28adba3ef2953c6f83e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78d8247320b0352399850d03971672c",
+    "url": "/red-calendar/index.html"
+  },
+  {
+    "revision": "d189eafe9e70b6f505ef",
+    "url": "/red-calendar/static/css/main.15a34566.chunk.css"
+  },
+  {
+    "revision": "068fabb2d8101b3fa566",
+    "url": "/red-calendar/static/js/2.58c2b442.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/red-calendar/static/js/2.58c2b442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d189eafe9e70b6f505ef",
+    "url": "/red-calendar/static/js/main.976f3b80.chunk.js"
+  },
+  {
+    "revision": "4c01eb266f4538fad257",
+    "url": "/red-calendar/static/js/runtime-main.6da66228.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/red-calendar/precache-manifest.3529aca3a33a28adba3ef2953c6f83e6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/red-calendar/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
